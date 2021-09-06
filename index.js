580fabe3a367bab6542c98634063637ad257fada
Joshua <joshuawilliams7800@gmail.com>
2021-09-06 17:59:37 -0500

    Add all updated files

---
+++ index.js
@@ -0,0 +1,56 @@
+const div = document.querySelector('div')
+const button = document.querySelector('button')
+const input = document.createElement('input')
+const para = document.querySelector('p')
+const h2 = document.querySelector('h2')
+const newH2 = document.createElement('h2')
+const newBtn = document.createElement('button')
+const newP = document.createElement('p')
+
+
+const secretNum = () => {
+   const num = prompt("What is your Secret Number?", '1 - 10')
+       // if secretnum is greater than 10 then alert its too high
+       if (num > 10) {
+        alert("That's too high!")
+        // if secretnum is less that 0 alert its too low
+    } else if (num < 1) {
+        alert("That's too low!")
+    } else {
+    // remove button after obtaining secretnum
+    // remove paragraph element
+    // append input
+        button.remove()
+        para.remove()
+        h2.remove()
+        div.append(input)
+        console.log(num)
+    }
+// check for equality between input and secretNum's value
+    input.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter') {
+            if(input.value < num) {
+                newH2.innerText = 'go up'
+                div.append(newH2)
+                input.value = ''
+            } else if(input.value > num) {
+                newH2.innerText = 'go down'
+                div.append(newH2)
+                input.value = ''
+            } else if(input.value === num) {
+                newH2.innerText = 'YOU WON!!!! I knew you could do it!'
+                div.append(newH2)
+                newP.innerText = 'refresh the browser to play again...'
+                div.append(newP)
+                input.value = ''
+            }
+            }
+        }
+    )}
+
+// run secretnum prompt when start game button is clicked
+button.addEventListener('click', () => {
+    secretNum()
+    }
+)
+
